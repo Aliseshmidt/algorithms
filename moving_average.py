b2455817ac74484bd440f4316a188aa58909023e
Aliseshmidt <chagelova84@gmail.com>
2024-07-04 23:40:08 +0300

    Optimal decision

--- moving_average.py
+++ moving_average.py
@@ -3,9 +3,11 @@ arr1 = list(map(int, input().split()))
 win = int(input()) 
 arr2=[]
 
+current_sum = sum(arr1[:win])
+arr2.append(current_sum/win)
 
-for i in range(n-win+1):
-    current_sum = sum(arr1[i:i+win])
+for i in range(1, n-win+1):
+    current_sum = current_sum - arr1[i-1] + arr1[i + win - 1]
     arr2.append(current_sum/win)
 
 print(" ".join(str(x) for x in arr2))
