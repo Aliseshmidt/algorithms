4c6ff332e60d3d4545ce472badf63b750cb95086
Aliseshmidt <chagelova84@gmail.com>
2024-07-04 23:18:38 +0300

    Trying to optimize for speed by def

--- moving_average.py
+++ moving_average.py
@@ -1,8 +1,13 @@
-n = int(input())
+n = int(input()) 
 arr1 = list(map(int, input().split()))
-win = int(input())
+win = int(input()) 
 arr2=[]
+
+def summ(arr, win, i):
+    return sum(arr[i:i+win])
+
+
 for i in range(n-win+1):
-    arr2.append(sum(arr1[i:i+win])/win)
+    arr2.append(summ(arr1, win, i)/win)
 
 print(" ".join(str(x) for x in arr2))
