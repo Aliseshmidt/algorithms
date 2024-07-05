72c48db2cf0a68622498b69ced1ac0c02a16addb
Aliseshmidt <chagelova84@gmail.com>
2024-07-05 22:19:30 +0300

    Insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,7 +1,15 @@
 unsort_arr = list(map(int, input().split()))
 
 def insertion_sort(arr):
-    for i in range(len(arr)):
-        if arr[i] > arr[i+1]:
-            curr = arr[i+1]
-            for j in range()
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+            print(i, j , arr)
+        arr[j + 1] = key
+        print(arr)
+    return arr
+
+print(insertion_sort(unsort_arr))
